9c0410fb76c2d080a82d035e68d9d7c55a643d75
Mike <mikhaz.castillo@gmail.com>
2022-01-12 21:17:11 +0800

    updates

---
+++ withdraw.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+lastcol='| sed -r "s/.* //g"'
+providerid="$(myst cli identities list | awk '{print $NF}')"
+providerid=$(echo "$providerid" | cut -c5-100)
+balance=$(myst cli identities get "$providerid" | grep Balance: | sed -r "s/.* //g")
+walletaddr=$(myst cli identities get-payout-address "$providerid" | sed -r "s/.* //g")
+#walletaddr=$(echo "$walletaddr" | cut -c5-100)
+
+echo $balance
+echo $providerid
+echo $walletaddr
+
+timewithdraw=$(echo "$balance >= 1" | bc)
+if [ $timewithdraw -ge 1 ]; then 
+  echo "withdraw time!"
+  echo $(myst cli identities withdraw "$providerid" "$walletaddr" 137)
+fi
+# 1 - Ethereum network
+# 137 - Matic/Polygon
